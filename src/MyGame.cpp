e51f93cb28624797659558bf1ea0264d005e9b0f
Kai Nicholas <mail@kainicholas.co.uk>
2023-10-23 18:43:24 +0100

    initial commit

---
+++ src/MyGame.cpp
@@ -0,0 +1,86 @@
+#include "MyGame.h"
+
+void MyGame::on_receive(std::string cmd, std::vector<std::string>& args) {
+    if (cmd == "GAME_DATA") {
+        // we should have exactly 4 arguments
+        if (args.size() == 4) {
+            game_data.player1Y = stoi(args.at(0));
+            game_data.player2Y = stoi(args.at(1));
+            game_data.ballX = stoi(args.at(2));
+            game_data.ballY = stoi(args.at(3));
+        }
+    } else if (cmd == "SCORES") {
+        // we should have exactly 2 arguments
+        if (args.size() == 2) {
+            scores.player1Score = stoi(args.at(0));
+            scores.player2Score = stoi(args.at(1));
+        }
+    }
+    else {
+        std::cout << "Received: " << cmd << std::endl;
+    }
+}
+
+void MyGame::send(std::string message) {
+    messages.push_back(message);
+}
+
+void MyGame::input(SDL_Event& event) {
+    switch (event.key.keysym.sym) {
+        case SDLK_w:
+            send(event.type == SDL_KEYDOWN ? "W_DOWN" : "W_UP");
+            break;
+        case SDLK_s:
+            send(event.type == SDL_KEYDOWN ? "S_DOWN" : "S_UP");
+            break;
+        case SDLK_i:
+            send(event.type == SDL_KEYDOWN ? "I_DOWN" : "I_UP");
+            break;
+        case SDLK_k:
+            send(event.type == SDL_KEYDOWN ? "K_DOWN" : "K_UP");
+            break;
+    }
+}
+
+void MyGame::update() {
+    player1.y = game_data.player1Y;
+    player2.y = game_data.player2Y;
+
+    ball.x = game_data.ballX;
+    ball.y = game_data.ballY;
+
+    player1Score = scores.player1Score;
+    player2Score = scores.player2Score;
+}
+
+void MyGame::render(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    
+    SDL_RenderDrawRect(renderer, &player1);
+    SDL_RenderDrawRect(renderer, &player2);
+
+    SDL_RenderDrawRect(renderer, &ball);
+
+    // Create a surface from the text
+    std::string player1ScoreText = std::to_string(player1Score);
+    SDL_Surface* player1ScoreTextSurface = TTF_RenderText_Solid(scoreFont, player1ScoreText.c_str(), scoreColor);
+    SDL_Texture* player1ScoreTextTexture = SDL_CreateTextureFromSurface(renderer, player1ScoreTextSurface);
+
+    std::string player2ScoreText = std::to_string(player2Score);
+    SDL_Surface* player2ScoreTextSurface = TTF_RenderText_Solid(scoreFont, player2ScoreText.c_str(), scoreColor);
+    SDL_Texture* player2ScoreTextTexture = SDL_CreateTextureFromSurface(renderer, player2ScoreTextSurface);
+
+    // Define a rect for the text position
+    SDL_Rect player1ScoreTextRect = { GAME_WIDTH / 4 - 50, 110, player1ScoreTextSurface->w, player1ScoreTextSurface->h };
+    SDL_Rect player2ScoreTextRect = { 3 * GAME_WIDTH / 4 - 20 + 40, 110, player2ScoreTextSurface->w, player2ScoreTextSurface->h };
+
+    // Render the text
+    SDL_RenderCopy(renderer, player1ScoreTextTexture, NULL, &player1ScoreTextRect);
+    SDL_RenderCopy(renderer, player2ScoreTextTexture, NULL, &player2ScoreTextRect);
+
+    SDL_FreeSurface(player1ScoreTextSurface);
+    SDL_DestroyTexture(player1ScoreTextTexture);
+
+    SDL_FreeSurface(player2ScoreTextSurface);
+    SDL_DestroyTexture(player2ScoreTextTexture);
+}
---
+++ src/MyGame.h
@@ -0,0 +1,49 @@
+#ifndef __MY_GAME_H__
+#define __MY_GAME_H__
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "SDL.h"
+#include "SDL_ttf.h"
+
+static int GAME_WIDTH = 800;
+
+static struct GameData {
+    int player1Y = 0;
+    int player2Y = 0;
+    int ballX = 0;
+    int ballY = 0;
+} game_data;
+
+static struct Scores {
+    int player1Score = 0;
+    int player2Score = 0;
+} scores;
+
+class MyGame {
+
+    private:
+        int player1Score = 0;
+        int player2Score = 0;
+
+        SDL_Color scoreColor = { 255, 255, 255 }; // White color
+
+        SDL_Rect player1 = { GAME_WIDTH / 4, 0, 20, 60 };
+        SDL_Rect player2 = { 3 * GAME_WIDTH / 4 - 20, 0, 20, 60 };
+
+        SDL_Rect ball = { 0, 0, 20, 20 };
+
+    public:
+        TTF_Font* scoreFont;
+        std::vector<std::string> messages;
+
+        void on_receive(std::string message, std::vector<std::string>& args);
+        void send(std::string message);
+        void input(SDL_Event& event);
+        void update();
+        void render(SDL_Renderer* renderer);
+};
+
+#endif
